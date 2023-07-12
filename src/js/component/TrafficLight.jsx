844f64d2c248581cdc6b5505c88a811218bc0fc0
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-07-12 12:48:23 +0000

    Improved and simplify logic

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,53 +1,60 @@
 import React, { useState } from "react";
 
+// Create and destructure light initial values.
+const lightColors = ['red-light', 'yellow-light', 'green-light'];
+const [red, yellow, green] = lightColors;
+
+// Create and destructure glow values.
+const glowColors = ['red-light-glow', 'yellow-light-glow', 'green-light-glow'];
+const [redGlow, yellowGlow, greenGlow] = glowColors;
+
+export const TrafficLight = ({ handleTraficImage }) => {
+    // State of lights
+    const [redLight, setRedLight] = useState(redGlow);
+    const [yellowLight, setYellowLight] = useState(yellow);
+    const [greenLight, setGreenLight] = useState(green);
+
+    // Form onSubmit function.
+    const handleLight = (event) => {
+        // Prevent the browser from refresh.
+        event.preventDefault();
+        // Reset lights classes.
+        setRedLight(red);
+        setYellowLight(yellow);
+        setGreenLight(green);
+
+        // Find which light was clicked and add new class.
+        if (event.nativeEvent.submitter.className === red) setRedLight(redGlow);
+
+        if (event.nativeEvent.submitter.className === yellow) setYellowLight(yellowGlow);
+
+        if (event.nativeEvent.submitter.className === green) setGreenLight(greenGlow);
+
+        // Send information to Home component with the light is currently on.
+        handleTraficImage(event.nativeEvent.submitter.className)
+
+    };
 
 
-
-export const TrafficLight = () => {
-
-    const [redLight, setRedLight] = useState('red-light');
-    const [yellowLight, setYellowLight] = useState('yellow-light');
-    const [greenLight, setGreenLight] = useState('green-light');
-
-    const handleRedLight = () => {
-        if (redLight === "red-light") {
-            setRedLight("red-light-glow")
-            setYellowLight("yellow-light")
-            setGreenLight("green-light")
-        } else {
-            setRedLight("red-light")
-        }
-
-    }
-    const handleYellowLight = () => {
-        if (yellowLight === "yellow-light") {
-            setYellowLight("yellow-light-glow")
-            setRedLight("red-light")
-            setGreenLight("green-light")
-        } else {
-            setYellowLight("yellow-light")
-        }
-    }
-    const handleGreenLight = () => {
-
-        if (greenLight === "green-light") {
-            setGreenLight("green-light-glow")
-            setRedLight("red-light")
-            setYellowLight("yellow-light")
-        } else {
-            setGreenLight("green-light")
-        }
-    }
 
     return (
         <>
-            <div className="card d-flex flex-column  align-items-center bg-dark" style={{ width: "10rem" }}>
-                <div className={redLight} onClick={handleRedLight}></div>
-                <div className={yellowLight} onClick={handleYellowLight}></div>
-                <div className={greenLight} onClick={handleGreenLight}></div>
+            <div className="" style={{ width: "10rem" }}>
+                <form action="" onSubmit={handleLight} className="bg-dark card d-flex flex-column  align-items-center">
+                    <div className="m-2">
+                        <button className={redLight} ></button>
+                    </div>
+                    <div className="m-2">
+                        <button className={yellowLight} ></button>
+                    </div>
+                    <div className="m-2">
+                        <button className={greenLight} ></button>
+                    </div>
+                </form>
+
             </div>
 
         </>
     )
-}
+};
 
